75f7d4fd27e1aa5248240641dabcffe282230dc6
jaggerabney <jagger.abney@gmail.com>
2023-02-04 19:27:45 -0800

    completed question #205

--- leetcode-75/level-1/questions.js
+++ leetcode-75/level-1/questions.js
@@ -37,3 +37,37 @@ function pivotIndex(nums) {
 
   return -1;
 }
+
+// Day 2
+
+// #205 - Isomorphic Strings
+
+function isIsomorphic(s, t) {
+  const sChars = s.split("");
+  const tChars = t.split("");
+  const dict = new Map();
+  let sChar = "",
+    tChar = "",
+    currentResult = "";
+
+  // create a map between the chars of s and t
+  for (let i = 0; i < sChars.length; i++) {
+    sChar = sChars[i];
+    tChar = tChars[i];
+
+    if (!dict.has(sChar)) {
+      if (currentResult.includes(tChar)) {
+        return false;
+      }
+
+      dict.set(sChar, tChar);
+      currentResult += tChar;
+    }
+  }
+
+  const result = sChars.map((char) => dict.get(char)).join("");
+
+  console.log(result, t);
+
+  return result.localeCompare(t) === 0;
+}
